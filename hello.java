550e7a6090da2d8751bf03dd7982d066c6ddf5c4
CalebJones94 <31103185+CalebJones94@users.noreply.github.com>
2017-10-23 17:12:32 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,11 +1,9 @@
-// a simple program to practice common Github commands 
 
 public class hello {
 	import java.util.Scanner;
     public static void main(String [] args) {
 
 
-	// added  line	
 	System.out.print("What is your name? ");
 	String Name = new Scanner(System.in);
         System.out.println("Hello " + Name + " My name is Caleb!");
